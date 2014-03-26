425dae8e193d95127b52241d9c059abf1b291d0e
mjcole90 <mjcole@btinternet.com>
2014-03-26 07:36:31 +0000

    All of the current website

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewRequests()
+        {
+            return View();
+        }
+
+        public ActionResult CreateRequest()
+        {
+            return View();
+        }
+
+        public ActionResult AdHocRequest()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+           
+            return View();
+        }
+
+         public ActionResult Help()
+        {
+            return View();
+        }
+	}
+}
---
+++ WebApplication1/Models/loginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace WebApplication1.Controllers
+{
+     public class loginModel
+    {
+         [Required]
+         public string Username { get; set; }
+         [Required]
+         public string Password { get; set; }
+    }
+}
